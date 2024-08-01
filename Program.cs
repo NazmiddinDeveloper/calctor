bf4f6e288f624e0a9c90ab4f843e06390dfa5023
nazmiddin <nazmiddinsirojiddinov080@gmail.com>
2024-08-01 15:53:57 +0500

    snova addad base and % mini feature

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
+﻿﻿// calculator programm 
+int number1;
+int number2;
+string userName;
+Console.Write("Hello user please type your name : ");
+userName = Console.ReadLine();
+Console.Write($"{userName} please enter first number : ");
+number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{userName} please enter second number : ");
+number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{number1} + {number2} = {number1+number2}");
+Console.WriteLine($"{number1} - {number2} = {number1-number2}");
+Console.WriteLine($"{number1} * {number2} = {number1*number2}");
+Console.WriteLine($"{number1} / {number2} = {number1/number2}");
+
 Console.WriteLine($"{number1} % {number2} = {number1%number2}");
